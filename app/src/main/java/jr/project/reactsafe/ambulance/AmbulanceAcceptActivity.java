56458f54e1f922cad0a8fc6429211f3463f3d3f1
Joraj J R <myselfjoraj@gmail.com>
2024-03-29 15:03:57 +0530

    new commit

--- app/src/main/java/jr/project/reactsafe/ambulance/AmbulanceAcceptActivity.java
+++ app/src/main/java/jr/project/reactsafe/ambulance/AmbulanceAcceptActivity.java
@@ -1,31 +1,249 @@
 package jr.project.reactsafe.ambulance;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 import jr.project.reactsafe.R;
 import jr.project.reactsafe.databinding.ActivityAmbulanceAcceptBinding;
+import jr.project.reactsafe.extras.misc.DirectionsJSONParser;
+import jr.project.reactsafe.extras.util.Extras;
+import jr.project.reactsafe.parent.ParentAccidentProceedings;
 
 public class AmbulanceAcceptActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     ActivityAmbulanceAcceptBinding binding;
+    GoogleMap mMap;
+    LatLng cLoc = new LatLng(37.0902, 95.7129);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ambulance_accept);
+        binding = ActivityAmbulanceAcceptBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapView);
+        if (mapFragment != null)
+            mapFragment.getMapAsync(this);
     }
 
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap = googleMap;
+        googleMap.addMarker(new MarkerOptions()
+                .position(cLoc)
+                .title("You")
+                .icon(Extras.bitmapFromVector(getApplicationContext(),R.drawable.marker_map_icon)));
+        googleMap.moveCamera(
+                CameraUpdateFactory.newLatLngZoom(cLoc,15));
+    }
 
+    private void setPolyLineInMap(String lat, String lng, String hLat, String hLng) {
+        if (lat == null || lng == null)
+            return;
+        mMap.clear();
+        LatLng myLoc = new LatLng(Double.parseDouble(lat),Double.parseDouble(lng));
+        LatLng toLoc = new LatLng(Double.parseDouble(hLat),Double.parseDouble(hLng));
+        mMap.addMarker(new MarkerOptions()
+                .position(myLoc)
+                .title("Child")
+                .icon(Extras.bitmapFromVector(getApplicationContext(),R.drawable.marker_map_icon)));
+        mMap.addMarker(new MarkerOptions()
+                .position(toLoc)
+                .title("Hospital")
+                .icon(Extras.bitmapFromVector(getApplicationContext(),R.drawable.marker_map_icon)));
+        mMap.moveCamera(
+                CameraUpdateFactory.newLatLngZoom(myLoc,11));
+
+
+        String url = getDirectionsUrl(toLoc, myLoc);
+
+        DownloadTask downloadTask = new DownloadTask();
+
+        // Start downloading json data from Google Directions API
+        downloadTask.execute(url);
+
+    }
+
+    private class DownloadTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... url) {
+
+            String data = "";
+
+            try {
+                data = downloadUrl(url[0]);
+            } catch (Exception e) {
+                Log.d("Background Task", e.toString());
+            }
+            return data;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            ParserTask parserTask = new ParserTask();
+
+
+            parserTask.execute(result);
+
+        }
+    }
+
+
+    /**
+     * A class to parse the Google Places in JSON format
+     */
+    private class ParserTask extends AsyncTask<String, Integer, List<List<HashMap<String, String>>>> {
+
+        // Parsing the data in non-ui thread
+        @Override
+        protected List<List<HashMap<String, String>>> doInBackground(String... jsonData) {
+
+            JSONObject jObject;
+            List<List<HashMap<String, String>>> routes = null;
+
+            try {
+                jObject = new JSONObject(jsonData[0]);
+                DirectionsJSONParser parser = new DirectionsJSONParser();
+
+                routes = parser.parse(jObject);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return routes;
+        }
+
+        @Override
+        protected void onPostExecute(List<List<HashMap<String, String>>> result) {
+            ArrayList points = null;
+            PolylineOptions lineOptions = new PolylineOptions();
+            MarkerOptions markerOptions = new MarkerOptions();
+
+            for (int i = 0; i < result.size(); i++) {
+                points = new ArrayList();
+
+                List<HashMap<String, String>> path = result.get(i);
+
+                for (int j = 0; j < path.size(); j++) {
+                    HashMap<String, String> point = path.get(j);
+
+                    double lat = Double.parseDouble(point.get("lat"));
+                    double lng = Double.parseDouble(point.get("lng"));
+                    LatLng position = new LatLng(lat, lng);
+
+                    points.add(position);
+                }
+
+                lineOptions.addAll(points);
+                lineOptions.width(12);
+                lineOptions.color(ContextCompat.getColor(AmbulanceAcceptActivity.this,R.color.react_safe));
+                lineOptions.geodesic(true);
+
+            }
+
+// Drawing polyline in the Google Map for the i-th route
+            mMap.addPolyline(lineOptions);
+        }
+    }
+
+    private String getDirectionsUrl(LatLng origin, LatLng dest) {
+
+        // Origin of route
+        String str_origin = "origin=" + origin.latitude + "," + origin.longitude;
+
+        // Destination of route
+        String str_dest = "destination=" + dest.latitude + "," + dest.longitude;
+
+        // Sensor enabled
+        String sensor = "sensor=false";
+        String mode = "mode=driving";
+        // Building the parameters to the web service
+        String parameters = str_origin + "&" + str_dest + "&" + sensor + "&" + mode;
+
+        // Output format
+        String output = "json";
+
+        // Building the url to the web service
+        String url = "https://maps.googleapis.com/maps/api/directions/" + output + "?" + parameters;
+
+        url = "https://maps.googleapis.com/maps/api/directions/json?"+str_origin +"&"+
+                str_dest +
+                "&sensor=false" +
+                "&mode=driving" +
+                "&key="+ ContextCompat.getString(this,R.string.google_maps_key);
+
+        Log.e("MapUrl",url);
+
+
+        return url;
+    }
+
+    /**
+     * A method to download json data from url
+     */
+    private String downloadUrl(String strUrl) throws IOException {
+        String data = "";
+        InputStream iStream = null;
+        HttpURLConnection urlConnection = null;
+        try {
+            URL url = new URL(strUrl);
+
+            urlConnection = (HttpURLConnection) url.openConnection();
+
+            urlConnection.connect();
+
+            iStream = urlConnection.getInputStream();
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(iStream));
+
+            StringBuffer sb = new StringBuffer();
+
+            String line = "";
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+
+            data = sb.toString();
+
+            br.close();
+
+        } catch (Exception e) {
+            Log.d("Exception", e.toString());
+        } finally {
+            iStream.close();
+            urlConnection.disconnect();
+        }
+        return data;
     }
 }
