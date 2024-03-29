18c04494ce2c27b7bc507de336978f0898358d30
Joraj J R <myselfjoraj@gmail.com>
2024-03-29 13:55:30 +0530

    new commit

--- app/src/main/java/jr/project/reactsafe/ambulance/AmbulanceMainActivity.java
+++ app/src/main/java/jr/project/reactsafe/ambulance/AmbulanceMainActivity.java
@@ -1,6 +1,7 @@
 package jr.project.reactsafe.ambulance;
 
 import android.os.Bundle;
+import android.widget.CompoundButton;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -13,6 +14,7 @@ import androidx.core.view.WindowInsetsCompat;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
@@ -49,8 +51,29 @@ public class AmbulanceMainActivity extends AppCompatActivity {
         accepted = new ArrayList<>();
         pending  = new ArrayList<>();
 
-        FirebaseDatabase.getInstance().getReference().child("ambulance")
-                .child(uid).child("alert").addValueEventListener(new ValueEventListener() {
+        DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference().child("ambulance").child(uid);
+        dbRef.child("isActive").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()){
+                    boolean isActive = Boolean.TRUE.equals(snapshot.getValue(Boolean.class));
+                    binding.start.setChecked(isActive);
+                }else {
+                    binding.start.setChecked(false);
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {}});
+
+        binding.start.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                dbRef.child("isActive").setValue(isChecked);
+            }
+        });
+
+
+        dbRef.child("alert").addValueEventListener(new ValueEventListener() {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot snapshot1) {
                         accepted.clear();
