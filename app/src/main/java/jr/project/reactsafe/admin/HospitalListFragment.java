c93397d6e2ddc298180c915fee4e46efb2ea230a
Joraj J R <myselfjoraj@gmail.com>
2024-04-03 06:40:21 +0530

    new commit

--- app/src/main/java/jr/project/reactsafe/admin/HospitalListFragment.java
+++ app/src/main/java/jr/project/reactsafe/admin/HospitalListFragment.java
@@ -10,11 +10,6 @@ import android.view.ViewGroup;
 
 import jr.project.reactsafe.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HospitalListFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class HospitalListFragment extends Fragment {
 
     public HospitalListFragment() {
